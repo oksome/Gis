7bc855aa374f37e79d6aeae8a480dd33c5b97a63
OKso <user+github@okso.me>
2014-02-17 18:01:46 +0100

    Added delete last commit, undo last commit

--- gis.py
+++ gis.py
@@ -7,6 +7,12 @@ argv = sys.argv[1:]
 
 if argv == ['s']:
     system('git status')
+elif argv == ['delete', 'last', 'commit']:
+    # nuke last commit and never see it again
+    system('git reset --hard HEAD~1')
+elif argv == ['undo', 'last', 'commit']:
+    # undo the commit but keep your changes for a bit of editing before you do a better commit
+    system('git reset HEAD~1')
 else:
     print('Unknown command')
 
