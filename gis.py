1e2d9097f03be14448af07060f8ff83323a83fcb
OKso <user+github@okso.me>
2014-08-08 17:24:19 +0200

    Added doc, renamed 'update last commit' to 'edit last commit'

--- gis.py
+++ gis.py
@@ -15,10 +15,19 @@ elif argv == ['delete', 'last', 'commit']:
 elif argv == ['undo', 'last', 'commit']:
     # undo the commit but keep your changes for a bit of editing before you do a better commit
     system('git reset HEAD~1')
-elif argv == ['reset everything']:
+elif argv == (['reset', 'everything'], ['nuke']):
     system('git git reset --hard HEAD')
-elif argv == ['update', 'last', 'commit']:
+elif argv == ['edit', 'last', 'commit']:
     system('git commit --amend')
 else:
-    print('Unknown command')
+    print('''Unknown command.
+
+Available commands | alias:
+    status             | s
+    diff               | d
+    delete last commit
+    undo last commit
+    reset everything   | nuke
+    edit last commit
+    ''')
 
